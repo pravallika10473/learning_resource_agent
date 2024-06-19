1fbf2b8ccc291d9655f88b031149282afa9328d3
Pravallika Abbineni <pravallikaabbineni@gmail.com>
2024-06-18 18:23:03 -0600

    added new initial file react.py which uses ReAct framework of agentic workflow to the task of searching you-tube

---
+++ react.py
@@ -0,0 +1,211 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import re
+import httpx
+import json
+from function import search_youtube_videos
+import argparse
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the API key from environment variables
+api_key = os.getenv('OPENAI_API_KEY')
+
+# Initialize the OpenAI client with the API key
+client = OpenAI(api_key=api_key)
+
+class Agent:
+    def __init__(self, system=""):
+        self.system = system
+        self.messages = []
+        if self.system:
+            self.messages.append({"role": "system", "content": system})
+
+    def __call__(self, message):
+        self.messages.append({"role": "user", "content": message})
+        result = self.execute()
+        self.messages.append({"role": "assistant", "content": result})
+        return result
+
+    def execute(self):
+        completion = client.chat.completions.create(
+                        model="gpt-3.5-turbo", 
+                        temperature=0,
+                        messages=self.messages)
+        return completion.choices[0].message.content
+    
+known_actions = {
+    "search_youtube": search_youtube_videos,
+}
+
+action_re = re.compile('^Action: (\w+): (.*)$')   # python regular expression to selection action
+system_message = """
+You run in a loop of Thought, Action, PAUSE, Observation.
+At the end of the loop you output an Answer
+Use Thought to describe your thoughts about the question you have been asked.
+Use Action to run one of the actions available to you - then return PAUSE.
+Observation will be the result of running those actions.
+
+Your available actions are:
+
+search_youtube:
+e.g. search_youtube: Back propogation by 3Blue1Brown
+Returns a summary of suitable links from searching Youtube
+
+Example session:
+
+Question:Who are the top 3 Ai researchesrs? Give resources from all three of them on back propogation ?
+Thought: I should first list the top 3 Ai researchers, then I should use those names in searching for resources on you-tube.
+Action: search_youtube: Back Propogation by Gefrey Hinton
+PAUSE
+
+You will be called again with this:
+
+Observation: [
+    {
+        "title": "Stanford Seminar - Can the brain do back-propagation? Geoffrey Hinton",
+        "description": "\"Can the brain do back-propagation?\" -  Geoffrey Hinton of Google & University of Toronto\n\nAbout the talk:\nDeep learning has been very successful for a variety of difficult perceptual tasks. This suggests that the sensory pathways in the brain might also be using back-propagation to ensure that lower cortical areas compute features that are useful to higher cortical areas. Neuroscientists have not taken this possibility seriously because there are so many obvious objections: Neurons do not communicate real numbers; the output of a neuron cannot represent both a feature of the world and the derivative of a cost function with respect to the neuron's output; the feedback connections to lower cortical areas that are needed to communicate error derivatives do not have the same weights as the feedforward connections; the feedback connections do not even go to the neurons from which the feedforward connections originate; there is no obvious source of labelled data. I will describe joint work with Timothy Lillicrap on ways of overcoming these objections.\n\nSupport for the Stanford Colloquium on Computer Systems Seminar Series provided by the Stanford Computer Forum.\n\nSpeaker Abstract and Bio can be found here: \nhttp://ee380.stanford.edu/Abstracts/160427.html\n\nColloquium on Computer Systems Seminar Series (EE380) presents the current research in design, implementation, analysis, and use of computer systems. Topics range from integrated circuits to operating systems and programming languages. It is free and open to the public, with new lectures each week.\n\nLearn more: http://bit.ly/WinYX5\n\n0:00 Introduction\n0:48 Online stochastic gradient descent\n2:43 Four reasons why the brain cannot do backprop\n5:20 Sources of supervision that allow backprop learning without a separate supervision signal\n8:18 The wake-sleep algorithm (Hinton et. al. 1995)\n12:15 New methods for unsupervised learning\n13:39 Conclusion about supervision signals\n14:03 Can neurons communicate real values?\n16:16 Statistics and the brain\n18:39 Big data versus big models\n23:32 Dropout as a form of model averaging\n24:53 Different kinds of noise in the hidden activities\n28:38 How are the derivatives sent backwards?\n30:18 A fundamental representational decision: temporal derivatives represent error derivatives\n32:24 An early use of the idea that temporal derivatives encode error derivatives (Hinton & McClelland, 1988)\n35:17 Combining STDP with reverse STDP\n37:02 If this is what is happening, what should neuroscientists see?\n39:22 What the two top-down passes achieve\n40:11 A way to encode the top-level error derivatives\n48:28 A consequence of using temporal derivatives to code error derivatives\n48:40 The next problem\n50:18 Now a miracle occurs\n56:44 Why does feedback alignment work?",
+        "link": "https://www.youtube.com/watch?v=VIRCybGgHts"
+    },
+    {
+        "title": "Lecture 3.4 \u2014 The backpropagation algorithm \u2014 [ Deep Learning | Geoffrey Hinton | UofT ]",
+        "description": "\ud83d\udd14 Stay Connected! Get the latest insights on Artificial Intelligence (AI) \ud83e\udde0, Natural Language Processing (NLP) \ud83d\udcdd, and Large Language Models (LLMs) \ud83e\udd16. Follow (https://twitter.com/mtnayeem) on Twitter \ud83d\udc26 for real-time updates, news, and discussions in the field. \n\nCheck out the following interesting papers. Happy learning!\n\nPaper Title: \"On the Role of Reviewer Expertise in Temporal Review Helpfulness Prediction\"\nPaper: https://aclanthology.org/2023.findings-eacl.125/\nDataset: https://huggingface.co/datasets/tafseer-nayeem/review_helpfulness_prediction\n\nPaper Title: \"Abstractive Unsupervised Multi-Document Summarization using Paraphrastic Sentence Fusion\"\nPaper: https://aclanthology.org/C18-1102/\n\nPaper Title: \"Extract with Order for Coherent Multi-Document Summarization\"\nPaper: https://aclanthology.org/W17-2407.pdf\n\nPaper Title: \"Paraphrastic Fusion for Abstractive Multi-Sentence Compression Generation\"\nPaper: https://dl.acm.org/doi/abs/10.1145/3132847.3133106\n\nPaper Title: \"Neural Diverse Abstractive Sentence Compression Generation\"\nPaper: https://link.springer.com/chapter/10.1007/978-3-030-15719-7_14",
+        "link": "https://www.youtube.com/watch?v=VCT1N0EsGj0"
+    },
+    {
+        "title": "Lecture 13.1 \u2014 The ups and downs of backpropagation \u2014 [ Deep Learning | Geoffrey Hinton | UofT ]",
+        "description": "\ud83d\udd14 Stay Connected! Get the latest insights on Artificial Intelligence (AI) \ud83e\udde0, Natural Language Processing (NLP) \ud83d\udcdd, and Large Language Models (LLMs) \ud83e\udd16. Follow (https://twitter.com/mtnayeem) on Twitter \ud83d\udc26 for real-time updates, news, and discussions in the field. \n\nCheck out the following interesting papers. Happy learning!\n\nPaper Title: \"On the Role of Reviewer Expertise in Temporal Review Helpfulness Prediction\"\nPaper: https://aclanthology.org/2023.findings-eacl.125/\nDataset: https://huggingface.co/datasets/tafseer-nayeem/review_helpfulness_prediction\n\nPaper Title: \"Abstractive Unsupervised Multi-Document Summarization using Paraphrastic Sentence Fusion\"\nPaper: https://aclanthology.org/C18-1102/\n\nPaper Title: \"Extract with Order for Coherent Multi-Document Summarization\"\nPaper: https://aclanthology.org/W17-2407.pdf\n\nPaper Title: \"Paraphrastic Fusion for Abstractive Multi-Sentence Compression Generation\"\nPaper: https://dl.acm.org/doi/abs/10.1145/3132847.3133106\n\nPaper Title: \"Neural Diverse Abstractive Sentence Compression Generation\"\nPaper: https://link.springer.com/chapter/10.1007/978-3-030-15719-7_14",
+        "link": "https://www.youtube.com/watch?v=lDFY8vQe6-g"
+    },
+    {
+        "title": "What is Back Propagation",
+        "description": "Learn about watsonx\u2192 https://ibm.biz/BdyEjK\n\nNeural networks are great for predictive modeling \u2014 everything from stock trends to language translations. But what if the answer is wrong, how do they \u201clearn\u201d to do better? Martin Keen explains that during a process called backward propagation, the generated output is compared to the expected output, and then the error contributed by each neuron (or \u201cnode\u201d) is examined. By adjusting the node\u2019s weights and biases, error is reduced and thus the overall accuracy improved.\n\nGet started for free on IBM Cloud \u2192 https://ibm.biz/sign-up-now\n\nSubscribe to see more videos like this in the future \u2192\u00a0http://ibm.biz/subscribe-now",
+        "link": "https://www.youtube.com/watch?v=S5AGN9XfPK4"
+    },
+    {
+        "title": "Lecture 7.2 \u2014 Training RNNs with back propagation \u2014 [ Deep Learning | Geoffrey Hinton | UofT ]",
+        "description": "\ud83d\udd14 Stay Connected! Get the latest insights on Artificial Intelligence (AI) \ud83e\udde0, Natural Language Processing (NLP) \ud83d\udcdd, and Large Language Models (LLMs) \ud83e\udd16. Follow (https://twitter.com/mtnayeem) on Twitter \ud83d\udc26 for real-time updates, news, and discussions in the field. \n\nCheck out the following interesting papers. Happy learning!\n\nPaper Title: \"On the Role of Reviewer Expertise in Temporal Review Helpfulness Prediction\"\nPaper: https://aclanthology.org/2023.findings-eacl.125/\nDataset: https://huggingface.co/datasets/tafseer-nayeem/review_helpfulness_prediction\n\nPaper Title: \"Abstractive Unsupervised Multi-Document Summarization using Paraphrastic Sentence Fusion\"\nPaper: https://aclanthology.org/C18-1102/\n\nPaper Title: \"Extract with Order for Coherent Multi-Document Summarization\"\nPaper: https://aclanthology.org/W17-2407.pdf\n\nPaper Title: \"Paraphrastic Fusion for Abstractive Multi-Sentence Compression Generation\"\nPaper: https://dl.acm.org/doi/abs/10.1145/3132847.3133106\n\nPaper Title: \"Neural Diverse Abstractive Sentence Compression Generation\"\nPaper: https://link.springer.com/chapter/10.1007/978-3-030-15719-7_14",
+        "link": "https://www.youtube.com/watch?v=4gOdNtVNZtk"
+    }
+]
+
+
+Action: search_youtube:Yoshua Bengio backpropagation
+PAUSE
+You will be called again with this:
+
+Observation:[
+    {
+        "title": "Neural backpropagation workshop 2019 - Yoshua Bengio",
+        "description": "This is the fourth and last talk of our half-day neural backpropagation workshop organized at Mila on December 2019. In this talk, Yoshua Bengio talks about towards biological analogues of backdrop.",
+        "link": "https://www.youtube.com/watch?v=4-wmpOjn6iU"
+    },
+    {
+        "title": "Yoshua Bengio \u2013 Credit assignment: beyond backpropagation",
+        "description": "[NIPS 2016] Autodiff Workshop\nhttps://autodiff-workshop.github.io/2016.html\nSpeaker: Yoshua Bengio, MILA, Universit\u00e9 de Montr\u00e9al\nSlides: https://autodiff-workshop.github.io/slides/YoshuaBengio-NIPSAutoDiffWorkshop10dec2016.key.pdf",
+        "link": "https://www.youtube.com/watch?v=z1GkvjCP7XA"
+    },
+    {
+        "title": "Yoshua Bengio - Deep learning and Backprop in the Brain (CCN 2017)",
+        "description": "Presented at Cognitive Computational Neuroscience (CCN) 2017 (http://www.ccneuro.org) held September 6-8, 2017.",
+        "link": "https://www.youtube.com/watch?v=W86H4DpFnLY"
+    },
+    {
+        "title": "Prof. Yoshua Bengio - Deep learning & Backprop in the Brain",
+        "description": "Yoshua Bengio is a Canadian computer scientist, most noted for his work on artificial neural networks and deep learning. Bengio received his Bachelor of Science, Master of Engineering and PhD from McGill University.\n\nRecorded: September 8, 2017",
+        "link": "https://www.youtube.com/watch?v=FhRW77rZUS8"
+    },
+    {
+        "title": "Yoshua Bengio Extra Footage 1: Brainstorm with students \ud83d\udd34",
+        "description": "\ud83e\udd96 Buy a life-sized Dinosaur: https://amzn.to/2YB2rjS\n\n* This channel is a participant in the Amazon Services LLC Associates Program, an affiliate advertising program designed to provide a means for sites to earn advertising fees by advertising and linking to amazon.com\n\nPlease consider leaving a like and subscribing if you enjoyed the content. It helps tremendously, thank you!\n\nContent by: VPRO\n\nSource: https://openbeelden.nl/media/1023078/Yoshua_Bengio_Extra_Footage_1_Brainstorm_with_students.en\n\nThis content is licensed under Creative Commons. Please visit: https://openbeelden.nl/media/1023078/Yoshua_Bengio_Extra_Footage_1_Brainstorm_with_students.en to see licensing information and check https://creativecommons.org/licenses/ for more information about the respective license(s).\n\nPublication Date: 18 February 2016\n\nDescription: Bengio walking to a classroom; talking with students at the Montreal Institute for Learning Algorithms on generalizing the principle of backpropagation\n\nContributor Information: Joshua Bengio",
+        "link": "https://www.youtube.com/watch?v=g9V-MHxSCcs"
+    }
+]
+
+Action: search_youtube:back propagation Andrew Ng
+PAUSE
+You will be called again with this:
+
+Observation:[
+    {
+        "title": "Forward and Backward Propagation in Neural Networks by Prof. Andrew NG",
+        "description": "",
+        "link": "https://www.youtube.com/watch?v=-Lavz_I4l2U"
+    },
+    {
+        "title": "Neural Networks Learning | ML-005 Lecture 9 | Stanford University | Andrew Ng",
+        "description": "Contents:\n\nCost function,\nBackpropagation Algorithm,\nBackpropagation Intuition,\nUnrolling Parameters,\nGradient Checking,\nRandom Initialization,\nPutting it together,\nAutonomous Driving,",
+        "link": "https://www.youtube.com/watch?v=UVjj2fHu9YU"
+    },
+    {
+        "title": "5.1.2 Backpropagation Algorithm by Andrew Ng",
+        "description": "Neural Networks : Learning\n\nMachine Learning - Stanford University | Coursera\nby Andrew Ng\n\nPlease visit Coursera site:\nhttps://www.coursera.org",
+        "link": "https://www.youtube.com/watch?v=mO7BpWmzT78"
+    },
+    {
+        "title": "Machine Learning by Andrew Ng _ Stanford University# 45 Backpropagation Algorithm",
+        "description": "About this Course\nMachine learning is the science of getting computers to act without being explicitly programmed. In the past decade, machine learning has given us self-driving cars, practical speech recognition, effective web search, and a vastly improved understanding of the human genome. Machine learning is so pervasive today that you probably use it dozens of times a day without knowing it. Many researchers also think it is the best way to make progress towards human-level AI. In this class, you will learn about the most effective machine learning techniques, and gain practice implementing them and getting them to work for yourself. More importantly, you'll learn about not only the theoretical underpinnings of learning, but also gain the practical know-how needed to quickly and powerfully apply these techniques to new problems. Finally, you'll learn about some of Silicon Valley's best practices in innovation as it pertains to machine learning and AI.\n\nThis course provides a broad introduction to machine learning, datamining, and statistical pattern recognition. Topics include: (i) Supervised learning (parametric/non-parametric algorithms, support vector machines, kernels, neural networks). (ii) Unsupervised learning (clustering, dimensionality reduction, recommender systems, deep learning). (iii) Best practices in machine learning (bias/variance theory; innovation process in machine learning and AI). The course will also draw from numerous case studies and applications, so that you'll also learn how to apply learning algorithms to building smart robots (perception, control), text understanding (web search, anti-spam), computer vision, medical informatics, audio, database mining, and other areas.\n\nhttps://www.coursera.org/learn/machine-learning/home/info",
+        "link": "https://www.youtube.com/watch?v=7dHc0Zy8PXM"
+    },
+    {
+        "title": "Backpropagation Intuition (C1W3L10)",
+        "description": "Take the Deep Learning Specialization: http://bit.ly/39mD6eI\nCheck out all our courses: https://www.deeplearning.ai\nSubscribe to The Batch, our weekly newsletter: https://www.deeplearning.ai/thebatch\n\nFollow us: \nTwitter: https://twitter.com/deeplearningai_\nFacebook: https://www.facebook.com/deeplearningHQ/\nLinkedin: https://www.linkedin.com/company/deeplearningai",
+        "link": "https://www.youtube.com/watch?v=yXcQ4B-YSjQ"
+    }
+]
+
+You then output:
+
+Answer: The top three AI researchers widely recognized for their contributions to the field are Geoffrey Hinton, Andrew Ng, and Yoshua Bengio. Below are some resources from each of them on the topic of backpropagation:
+
+### Geoffrey Hinton
+1. **[Stanford Seminar - Can the brain do back-propagation? Geoffrey Hinton](https://www.youtube.com/watch?v=VIRCybGgHts)**
+2. **[Lecture 3.4 — The backpropagation algorithm — [ Deep Learning | Geoffrey Hinton | UofT ]](https://www.youtube.com/watch?v=VCT1N0EsGj0)**
+3. **[Does the brain do backpropagation? CAN Public Lecture - Geoffrey Hinton](https://www.youtube.com/watch?v=qIEfJ6OBGj8)**
+
+### Andrew Ng
+1. **[Forward and Backward Propagation in Neural Networks by Prof. Andrew NG](https://www.youtube.com/watch?v=-Lavz_I4l2U)**
+2. **[Neural Networks Learning | ML-005 Lecture 9 | Stanford University | Andrew Ng](https://www.youtube.com/watch?v=UVjj2fHu9YU)**
+3. **[5.1.2 Backpropagation Algorithm by Andrew Ng](https://www.youtube.com/watch?v=mO7BpWmzT78)**
+
+### Yoshua Bengio
+1. **[Neural backpropagation workshop 2019 - Yoshua Bengio](https://www.youtube.com/watch?v=4-wmpOjn6iU)**
+2. **[Yoshua Bengio – Credit assignment: beyond backpropagation](https://www.youtube.com/watch?v=z1GkvjCP7XA)**
+3. **[Yoshua Bengio - Deep learning and Backprop in the Brain (CCN 2017)](https://www.youtube.com/watch?v=W86H4DpFnLY)**
+
+These resources should provide you with ample information on backpropagation from some of the leading minds in AI research.
+""".strip()
+
+def query(question, max_turns=5):
+    i = 0
+    bot = Agent(system_message)
+    next_prompt = question
+    while i < max_turns:
+        i += 1
+        result = bot(next_prompt)
+        print(result)
+        actions = [
+            action_re.match(a) 
+            for a in result.split('\n') 
+            if action_re.match(a)
+        ]
+        if actions:
+            # There is an action to run
+            action, action_input = actions[0].groups()
+            if action not in known_actions:
+                raise Exception("Unknown action: {}: {}".format(action, action_input))
+            print(" -- running {} {}".format(action, action_input))
+            observation = known_actions[action](action_input)
+            print("Observation:", observation)
+            next_prompt = "Observation: {}".format(observation)
+        else:
+            return
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("question", type=str, help="The question to query.")
+    # args = parser.parse_args()
+    
+    query("What are the subtopics I need to learn to implement backpropogation? and give resources to learn for each one of those subtopics")
